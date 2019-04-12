aec1f73e6b62b79d691811801cfd6e6af0a91f21
Brionjv <brion.jv@laposte.net>
2019-04-12 17:45:20 +0200

    Update 12/04/2019
    #17 Try to fix checksum like https://crccalc.com/ with "crc test" program
    Work only beetween values 0 to 255...

---
+++ Break Mii checksum/crc test/Form1.vb
@@ -0,0 +1,61 @@
+﻿Public Class Form1
+    Dim savedataArc As String
+    Dim number1 As String
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Public Sub GetCrc()
+        Dim crc As Integer = &H0 ' Starting value 
+        Dim Polynom As Integer = &H1021 ' As in X^16 + X^12 + X^5 + 1 
+        Dim bytes() As Byte = Nothing
+        Dim bit As Boolean
+        Dim c15 As Boolean
+        Dim i As Integer
+
+        ' Convert the parameter to a Byte-array:  
+            If IsNothing(bytes) Then
+                ReDim bytes(0)
+            Else
+                ReDim Preserve bytes(bytes.Length)
+            End If
+
+        bytes(bytes.Length - 1) = CByte(CStr(TextBox2.Text))
+
+
+        ' Calculate the CRC: 
+        For Each b As Byte In bytes
+            For i = 0 To 7
+                bit = ((b >> (7 - i) And 1)) '= 1) 
+                c15 = ((crc >> 15 And 1)) '= 1) 
+                crc <<= 1
+                If c15 Xor bit Then
+                    crc = crc Xor Polynom
+                End If
+            Next
+        Next
+
+        crc = crc And &HFFFF
+
+        ' De crc-variable is a decimal value. We return a Hex-value, so we need to convert this to hex. 
+        ' The 8 in the Convert-method means the Hex-base. 
+        Dim retVal As String = System.Convert.ToString(crc, 16)
+        TextBox1.Text = retVal
+        Return
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        GetCrc()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim open As New OpenFileDialog
+        open.Filter = "Text files|*.txt"
+        open.Title = "Open save savedataArc.txt"
+        open.ShowDialog()
+        savedataArc = open.FileName
+        Dim Reader As New PackageIO.Reader(savedataArc, PackageIO.Endian.Little)
+        Reader.Position = &H1C70
+        TextBox2.Text = Reader.ReadHexString(&H5E)
+    End Sub
+End Class
---
+++ Break Mii checksum/crc test/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Les informations générales relatives à un assembly dépendent de 
+' l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
+' associées à un assembly.
+
+' Passez en revue les valeurs des attributs de l'assembly
+
+<Assembly: AssemblyTitle("crc test")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("HP Inc.")> 
+<Assembly: AssemblyProduct("crc test")> 
+<Assembly: AssemblyCopyright("Copyright © HP Inc. 2019")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
+<Assembly: Guid("e3a65894-4104-4647-be7f-c91b92d198e3")> 
+
+' Les informations de version pour un assembly se composent des quatre valeurs suivantes :
+'
+'      Version principale
+'      Version secondaire 
+'      Numéro de build
+'      Révision
+'
+' Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut 
+' en utilisant '*', comme indiqué ci-dessous :
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
